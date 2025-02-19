d6a82dd1b7befa34a98f32f8ab62aaed927d960d
unforkableco <anton@unforkable.co>
2025-02-19 16:33:01 +0100

    fixing up service

--- scripts/setup_node.sh
+++ scripts/setup_node.sh
@@ -38,7 +38,7 @@ After=network.target
 
 [Service]
 User=ubuntu
-ExecStart=/usr/local/bin/substrate-node --chain $CHAIN_SPEC --name "Test network" --rpc-external --ws-external --rpc-port $RPC_PORT --ws-port $WS_PORT --pruning=archive
+ExecStart=/usr/local/bin/substrate-node --name "Test network" --rpc-external --rpc-port $RPC_PORT --ws-port $WS_PORT --pruning=archive --rpc-cors all
 Restart=always
 RestartSec=10
 LimitNOFILE=4096
